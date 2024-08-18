4da01bdf9cd3063ff968bdc7b112225b44c997cb
709macdonald <709macdonald@gmail.com>
2024-08-18 16:51:34 -0230

    Added styling for the page

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,3 +1,20 @@
 export default function SideBar() {
-  return <div> </div>;
+  return (
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            asdafgad awfjadj wkD QKJEF wjaf wqrk fjkaeg jka dfjks djkbf khw FKJE
+            ASlf jaf vjkad fjkv ajfd bvkja dKFLV AJKEF DBN VJKAS DVKJA SVK wjksd
+          </p>
+        </div>
+        <button>
+          <i className="fa-solid fa-arrow-right"></i>{" "}
+        </button>
+      </div>
+    </div>
+  );
 }
