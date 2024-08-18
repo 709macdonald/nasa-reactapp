46fe132cbcd96d05a99cce402e0107e977755c5c
709macdonald <709macdonald@gmail.com>
2024-08-18 16:26:28 -0230

    Added mars image

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h2>The Brutal Martian Landscape</h2>
+        <h1>APOD Project</h1>
+      </div>
+    </footer>
+  );
+}
