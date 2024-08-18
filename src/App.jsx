dcddac6a6db8e45327cff3c70c0e999bab807b21
709macdonald <709macdonald@gmail.com>
2024-08-18 18:17:52 -0230

    creating dynamic sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import SideBar from "./components/SideBar";
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+
   return (
     <>
       <Main />
-      <SideBar>asdasd</SideBar>
+      {showModal && <SideBar />}
       <Footer />
     </>
   );
