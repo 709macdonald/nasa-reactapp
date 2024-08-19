9aa607e040f40fb2948f5a8304c6962a74ee976e
709macdonald <709macdonald@gmail.com>
2024-08-18 21:41:07 -0230

    Displaying image and video

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,23 @@ function App() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url =
         "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
+
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log("Fetched from cache today");
+        return;
+      }
+      localStorage.clear();
+
       try {
         const res = await fetch(url);
         const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData));
         setData(apiData);
-        console.log(`DATA\n`, apiData);
+        console.log("Fetched from API today");
       } catch (err) {
         console.log(err.message);
       }
@@ -33,14 +45,16 @@ function App() {
   return (
     <>
       {data ? (
-        <Main />
+        <Main data={data} />
       ) : (
         <div className="loadingState">
           <i className="fa-solid fa-gear"></i>
         </div>
       )}
-      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
-      {data && <Footer handleToggleModal={handleToggleModal} />}
+      {showModal && (
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
+      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,24 @@
-export default function Main() {
+export default function Main(props) {
+  const { data } = props;
+
   return (
     <div className="imgContainer">
-      <img src="mars.png" alt="mars-demo-picture" className="bgImage" />
+      {data.media_type === "video" ? (
+        <iframe
+          src={data.url}
+          title={data.title || "APOD video"}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="bgVideo"
+        ></iframe>
+      ) : (
+        <img
+          src={data.hdurl}
+          alt={data.title || "bg-img"}
+          className="bgImage"
+        />
+      )}
     </div>
   );
 }
